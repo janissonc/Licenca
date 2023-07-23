ba450de6aa71ac2addb0eddf1681bf6957dff22b
Janisson <janisson.bass@gmail.com>
2023-07-22 22:28:28 -0300

    .

--- UI5/webapp/controller/sistema/SistemaEdit.controller.js
+++ UI5/webapp/controller/sistema/SistemaEdit.controller.js
@@ -94,112 +94,11 @@ sap.ui.define([
             oRouter.navTo("sistema");
         },
 
-        onSave:function(){
+        onHandlerEdit:function(){
             var token =  localStorage.getItem("token");
             var oRouter = this.getRouter();
             if(!token){
                 console.log("Usuario não logado");
-                this.oRouter.navTo("login");     
-            }
-            sap.ui.core.BusyIndicator.show(0);
-            var fragmentId = this.getView().createId("formSistema");
-            var oNmSistema = sap.ui.core.Fragment.byId(fragmentId, "nmSistema").getValue();
-            var oControlarUsuario = sap.ui.core.Fragment.byId(fragmentId, "controlarUsuario").getSelected();
-            var oTipoControle = sap.ui.core.Fragment.byId(fragmentId, "tipoControle").getSelectedButton().getText();
-          
-           
-
-            const sistemaObj = {
-                "nmSistema": oNmSistema,
-                "status": true,
-                "controlaUsuarios": oControlarUsuario
-            }
-           const url =  this.getURL("InsertSistema")
-          
-            $.ajax({
-                type: "PUT",
-                url: url,
-                data: JSON.stringify(sistemaObj),
-                //crossDomain: true,
-                headers: {'Token':token},
-                contentType: "application/json",
-                success: function (res) {
-                    sap.ui.core.BusyIndicator.hide(0);
-                    MessageToast.show("Adicionado com sucesso")
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    console.log("Got an error response: " + textStatus + errorThrown);
-                    var response = jqXHR.responseJSON;
-                    sap.ui.core.BusyIndicator.hide(0);
-                    if(response.status == false && response.responseCode == 401){
-                        oRouter.navTo("login");
-                    }
-                }
-            }).then(()=>{
-                sap.ui.core.BusyIndicator.hide(0);
-            });
-        },
-
-        handlerDelete:function(oEvent){
-            sap.ui.core.BusyIndicator.show(0);
-            var token =  localStorage.getItem("token");
-            if(!token){
-                var oRouter = this.getRouter();
-                console.log("Usuario não logado");
-                oRouter.navTo("login");     
-            }
-            var sObjectId =  oEvent.getSource().getBindingContext("sistemas").getObject();
-            let url = "";
-            if(sObjectId){
-                url =  this.getURL(`DeleteSistema/?cdSistema=${sObjectId.cdSistema}`)
-            }else{
-                sap.ui.core.BusyIndicator.hide(0);
-                MessageToast.show("Erro ao recuperar item para ser deletado")
-            }
-
-            MessageBox.warning("Deseja mesmo deletar esse dado? ", {
-                actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
-                emphasizedAction: MessageBox.Action.OK,
-                    onClose: function (sAction) {
-                        if(sAction == "OK"){
-                            $.ajax({
-                                type: "DELETE",
-                                url: url,
-                                data: JSON.stringify({}),
-                                //crossDomain: true,
-                                headers: {'Token':token},
-                                contentType: "application/json",
-                                success: function (res) {
-                                    sap.ui.core.BusyIndicator.hide(0);
-                                    MessageToast.show("Deletado com sucesso")
-                                },
-                                error: function (jqXHR, textStatus, errorThrown) {
-                                    console.log("Got an error response: " + textStatus + errorThrown);
-                                    var response = jqXHR.responseJSON;
-                                    sap.ui.core.BusyIndicator.hide(0);
-                                    if(response.status == false && response.responseCode == 401){
-                                        oRouter.navTo("login");
-                                    }
-                                }
-                            }).then(()=>{
-                                
-                                sap.ui.core.BusyIndicator.hide(0);
-                            });
-                        }else{
-                            sap.ui.core.BusyIndicator.hide(0);
-                        }
-                        
-                    }
-                });
-
-           
-        },
-
-        onHandlerEdit:function(){
-            var token =  localStorage.getItem("token");
-            if(!token){
-                console.log("Usuario não logado");
-                var oRouter = this.getRouter();
                 oRouter.navTo("login");     
             }
             sap.ui.core.BusyIndicator.show(0);
@@ -226,6 +125,7 @@ sap.ui.define([
                     
                     sap.ui.core.BusyIndicator.hide(0);
                     MessageToast.show("Alterado com sucesso")
+                    oRouter.navTo("sistema");
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
                     console.log("Got an error response: " + textStatus + errorThrown);
